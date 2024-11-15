1dc1a844a0b30cb8089a9f4e75150ea4f43c0eaa
dredoelpajaslocas <dredd69nh@gmail.com>
2024-11-15 11:31:00 -0400

    Add files via upload

---
+++ last_work/datosM.js
@@ -0,0 +1,11 @@
+function uuf(){
+      var nn = document.getElementById('aa').value;
+      var pp = document.getElementById('bb').value;
+      if (nn == "Andres") {
+        if (pp == "and123") {
+          alert("CORRECTO");{
+          location.href="menu.html";
+        }} else 
+          alert("INCORRECTO");
+        }
+      }
